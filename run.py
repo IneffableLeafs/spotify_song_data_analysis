d0dc95f62de54611cbc2670e4073d03876af82e5
Peter Twarecki <64752041+IneffableLeafs@users.noreply.github.com>
2020-08-29 10:00:06 -0400

    Working prototype
    Read spotipy github documentation and got a run function to work, now it's time to customize.

--- run.py
+++ run.py
@@ -1,8 +1,25 @@
-import os
-from spotify_client import SpotifyClient
+# import os
+#from spotify_client2 import SpotifyClient
 
-def run():
-	# Search the user's Spotify for liked songs
-	spotify_client = SpotifyClient(os.getenv("SPOTIFY_AUTH_TOKEN"))
-	track_info = spotify_client.audio_features()
-	print(track_info)
+#print("Test")
+#spotify_client = SpotifyClient()
+#track_info = spotify_client.get_song()
+#print(track_info)
+
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+os.environ["SPOTIPY_CLIENT_ID"] = "43dfe303cbce478e8c8952eed351e881"
+os.environ["SPOTIPY_CLIENT_SECRET"] = "3b8aa6ee50b64d9bb16ba09554b0b51e"
+os.environ["SPOTIPY_REDIRECT_URI"] = "http://127.0.0.1:9090"
+os.environ["SPOTIPY_CACHE_PATH"] = "/Users/peter/spotipy-cache"
+
+scope = "user-library-read"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
--- spotify_client.py
+++ spotify_client.py
@@ -2,8 +2,30 @@ import string
 import requests
 
 class SpotifyClient(object):
-	def __init__(self, BQDXQrUHOTDrG6rvF6xyZl8xhiSwXo33xV6t3uSEu1ixPh1daCGueZLAivAb2iY):
+	def __init__(self, api_key):
 		self.api_key = api_key
+
+
+	def get_song(self):
+		url = "https://api.spotify.com/v1/me/tracks"
+
+		response = requests.get(
+			url, 
+			headers={
+				"Accept": "application/json",
+				"Content-Type": "application/json",
+				#"Authorization": f"Bearer {self.api_key}"
+				"Authorization": "Bearer BQC0kSUyR3-mcAGchwgO-Hdwkci2FEtM6v94bZldYg8EUqQGJFzQTBPHPr5-MiZerL7B5Arp_VccXtMCF9eVinoEU3bXVDfqUg36uCTaEiXH9OwtzdNdPIbGC9TZCZbyiJNzFp6K9-1dzLLzL-ZKResR86zD3ZGj"
+			}
+		)
+
+		response_json = response.json()
+		tracks = [track for track in response_json['items']['id']]
+		print(f"Found {len(tracks)} from your spotify")
+
+		return tracks
+
+
 
 	def audio_features(self, id):
 		url = "https://api.spotify.com/v1/audio-features/{id}"
@@ -11,7 +33,7 @@ class SpotifyClient(object):
 		# we need a way to get the id's of different songs,
 		# most likely another function that returns a song id.
 		# we then loop the audio_features function inside the get_song()
-		
+
 
 
 		response = requests.get(
