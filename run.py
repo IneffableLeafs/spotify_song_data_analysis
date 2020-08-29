0e9b703f64efc1c0b98fbd1a77fa5b02f8b0483e
Peter Twarecki <64752041+IneffableLeafs@users.noreply.github.com>
2020-08-29 10:12:17 -0400

    Removed redundant code
    Removed redundant code from run.py and transfered the run2.py content to run.py and deleted the second run file.

--- run.py
+++ run.py
@@ -1,23 +1,15 @@
-# import os
-#from spotify_client2 import SpotifyClient
-
-#print("Test")
-#spotify_client = SpotifyClient()
-#track_info = spotify_client.get_song()
-#print(track_info)
-
-import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-os.environ["SPOTIPY_CLIENT_ID"] = "43dfe303cbce478e8c8952eed351e881"
-os.environ["SPOTIPY_CLIENT_SECRET"] = "3b8aa6ee50b64d9bb16ba09554b0b51e"
-os.environ["SPOTIPY_REDIRECT_URI"] = "http://127.0.0.1:9090"
-os.environ["SPOTIPY_CACHE_PATH"] = "/Users/peter/spotipy-cache"
+username = input("Enter your Spotify username: ")
 
-scope = "user-library-read"
+scope = 'user-library-read'
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="43dfe303cbce478e8c8952eed351e881",
+                                               client_secret="3b8aa6ee50b64d9bb16ba09554b0b51e",
+                                               redirect_uri="http://127.0.0.1:9090",
+                                               username=username,
+                                               scope="user-library-read"))
 
 results = sp.current_user_saved_tracks()
 for idx, item in enumerate(results['items']):
