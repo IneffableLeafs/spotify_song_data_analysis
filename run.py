647e3eacaff7c6dd845af3c6e2814bc4ea9e54ad
Peter Twarecki <64752041+IneffableLeafs@users.noreply.github.com>
2020-08-30 16:11:20 -0400

    Fixed song_id
    It turns out I was scraping the artist ids instead of the song ids, so now my audio_features function works as intended.

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 from spotify_client import SpotifyClient
 
 # prompt user for username
@@ -14,6 +14,10 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="43dfe303cbce478e8c8952
                                                username=username,
                                                scope=scope))
 
+# authenticate for audio features
+client_credentials_manager = SpotifyClientCredentials()
+sp2 = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
 # set the offset initially to zero
 offset = 0
 
@@ -23,9 +27,22 @@ max_songs = 372
 # make a list to put our song ids into
 song_ids = []
 
+print("Depending on the size of library, this may take a few minutes.")
 # we need to keep calling get_song until we get all the songs in the user's library.
 for song in range(offset, max_songs): 
 
 	SpotifyClient.get_song_ids(sp, offset, song_ids)
 	offset += 50
+
+# now we need to loop through each track and get the audio features for each track, storing each feature in a separate list.
+# we will consider the following features:
+danceability, energy, loudness, speechiness, acousticness, instrumentalness, liveness, valence, tempo = ([] for i in range(9))
+
+# now, we want to hand the get_audio_features function a single track at once, then add its features to the respective list:
+#for song_id in song_ids:
+
+for song_id in song_ids:
+	SpotifyClient.get_audio_features(sp2, song_id, danceability, energy, loudness, speechiness, acousticness, instrumentalness, liveness, valence, tempo)
+
+
 
--- spotify_client.py
+++ spotify_client.py
@@ -1,17 +1,29 @@
 class SpotifyClient(object):
 
+	# this function will return a list of the song ids of the user's spotify library
 	def get_song_ids(client, offset, song_ids):
 
 		# set our limit to 50 so each query gets 50 songs, the max for the spotipy function
 		limit = 50
-
 		results = client.current_user_saved_tracks(limit, offset)
 		for idx, item in enumerate(results['items']):
 			track = item['track']
 			#print(idx + offset, track['artists'][0]['id'])
-			song_ids.append(track['artists'][0]['id'])
+			song_ids.append(track['id'])
 
 		return song_ids
 
-	def audio_features():
-		pass
+	# this function will store nine audio features of all tracks in your library in separate lists
+	def get_audio_features(client, song_id, danceability, energy, loudness, speechiness, acousticness, instrumentalness, liveness, valence, tempo):
+		features = client.audio_features(song_id)
+
+		# add each feature to its respective list
+		danceability.append(features[0]['danceability'])
+		energy.append(features[0]['energy'])
+		loudness.append(features[0]['loudness'])
+		speechiness.append(features[0]['speechiness'])
+		acousticness.append(features[0]['acousticness'])
+		instrumentalness.append(features[0]['instrumentalness'])
+		liveness.append(features[0]['liveness'])
+		valence.append(features[0]['valence'])
+		tempo.append(features[0]['tempo'])
