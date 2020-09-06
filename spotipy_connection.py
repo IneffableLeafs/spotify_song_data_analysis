84ead74d9291222f57fff8f64415f7800d05a672
Peter Twarecki <64752041+IneffableLeafs@users.noreply.github.com>
2020-09-05 21:35:19 -0400

    final edits part 2
    removed client information and user information.

--- spotipy_connection.py
+++ spotipy_connection.py
@@ -2,9 +2,9 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 # type your client_id, client_secret, and username (put them between quotation marks):
-client_id = "43dfe303cbce478e8c8952eed351e881"
-client_secret = "3b8aa6ee50b64d9bb16ba09554b0b51e"
-username = "ineffableleaves"
+client_id = "-----------------------"
+client_secret = "----------------------"
+username = "------------------"
 
 
 scope = 'user-library-read user-read-recently-played playlist-modify-private playlist-read-private'
